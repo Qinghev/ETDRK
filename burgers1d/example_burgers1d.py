218375f4cf1a04ff9b78a6bae6cc7ec302de3201
Qinghev <38534045+Qinghev@users.noreply.github.com>
2025-05-22 20:42:26 +0800

    Update example_burgers1d.py

--- burgers1d/example_burgers1d.py
+++ burgers1d/example_burgers1d.py
@@ -1,5 +1,6 @@
 import numpy as np
-import matplotlib.pyplot as plt
+from burgers1d_etdrk import initial_GRF, burgers1d_etdrk4
+# from convergence import *
 
 xa, xb = -np.pi, np.pi
 nx = 1024
@@ -9,5 +10,5 @@ vis = 0.03
 T = 2.0
 dt = 1e-2
 
-u0 = initial_GRF(N=nx//2, m=0, gamma=2.5, tau=7, sigma=7**2, bctype='periodic', interval=interval)
+x, u0 = initial_GRF(N=nx//2, m=0, gamma=2.5, tau=7, sigma=7**2, bctype='periodic', interval=interval)
 _, _, uu = burgers1d_etdrk4(u0, interval, dt, int(T/dt), vis, show_plot=True)
