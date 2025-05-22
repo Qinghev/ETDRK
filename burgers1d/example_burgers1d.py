e8b2cf6d89afc86f020f8d5ae818b42eaf90a659
Qinghev <38534045+Qinghev@users.noreply.github.com>
2025-05-22 20:33:27 +0800

    Create example_burgers1d.py

---
+++ burgers1d/example_burgers1d.py
@@ -0,0 +1,13 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+xa, xb = -np.pi, np.pi
+nx = 1024
+x = np.linspace(xa, xb, nx, endpoint=False)
+interval = (xa, xb)
+vis = 0.03
+T = 2.0
+dt = 1e-2
+
+u0 = initial_GRF(N=nx//2, m=0, gamma=2.5, tau=7, sigma=7**2, bctype='periodic', interval=interval)
+_, _, uu = burgers1d_etdrk4(u0, interval, dt, int(T/dt), vis, show_plot=True)
